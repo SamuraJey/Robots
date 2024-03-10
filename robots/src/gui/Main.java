e55e7570df1daf43ee873a904a877fa0fe8a8f75
Sergey Z <cergeypro703@gmail.com>
2024-03-10 14:33:26 +0500

    Update of MainApplicationFrame.java

--- robots/src/gui/Main.java
+++ robots/src/gui/Main.java
@@ -1,18 +1,14 @@
 package gui;
 
 import java.awt.Frame;
-
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-public class RobotsProgram
+public class Main
 {
     public static void main(String[] args) {
       try {
-        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-//        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-//        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
       } catch (Exception e) {
         e.printStackTrace();
       }
